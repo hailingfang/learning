b010426d6079eb736aec0fa1d2d79b7045d7a3b3
Benjamin Fang <benjaminfang.ol@outlook.com>
2023-02-20 16:10:50 +0800

    c linux directory operations

--- cc/linux_directory_operation.c
+++ cc/linux_directory_operation.c
@@ -6,39 +6,61 @@
 #include <dirent.h>
 
 int
-main(void)
+main(int argc, char *argv[])
 {
-    char dirname[] = "testdir";
-    char fname[512];
-    int status = 0;
-    if (!access(dirname, F_OK)) {
-        printf("dir already exist\n");
-        DIR *dirp = opendir(dirname);
-        struct dirent *dp;
-        while ((dp = readdir(dirp)) != NULL) {
-            if (strcmp(".", dp->d_name) != 0 && strcmp("..", dp->d_name) != 0) {
-                printf("%s\n", dp->d_name);
-                strcpy(fname, dirname);
-                strcat(fname, "/");
-                strcat(fname, dp->d_name);
-                unlink(fname);  
-            }
-        }
-
-        status = rmdir(dirname);
-        if (status == -1) {
-            printf("rm dir failed.\n");
-        }
-        return 0; 
-        mkdir(dirname, S_IRWXU);
-    } else {
-        printf("mk new dir.\n");
-        mkdir(dirname, S_IRWXU);
+    if (argc != 3) {
+        fprintf(stderr, "Error, a paration and a directory name must given.\n");
     }
 
-    strcpy(fname, dirname);
-    strcat(fname, "/tfile");
-    FILE *fout = fopen(fname, "w");
-    fprintf(fout, "hello world.\n");
+    char fname[512];
+    const char *opr = argv[1];
+    const char *dirname = argv[2];
+
+    if (strcmp(opr, "c") == 0) {
+        if (access(dirname, F_OK)) {
+            mkdir(dirname, S_IRWXU);
+        } else {
+            printf("%s alread exist, clean files within it.\n", dirname);
+            DIR *dirp = opendir(dirname);
+            struct dirent *dp;
+            while ((dp = readdir(dirp)) != NULL) {
+                if (strcmp(".", dp->d_name) != 0 && strcmp("..", dp->d_name) != 0) {
+                    strcpy(fname, dirname);
+                    strcat(fname, "/");
+                    strcat(fname, "dp->d_name");
+                    if (unlink(fname)) {
+                        fprintf(stderr, "%s remove failed.\n", fname);
+                    } else {
+                        printf("%s removed.\n", fname);
+                    }
+                }
+            }
+            closedir(dirp);
+        }
+    } else if (strcmp(opr, "d") == 0) {
+        if (access(dirname, F_OK)) {
+            fprintf(stderr, "%s not exists.\n", dirname);
+        } else {
+            DIR *dirp = opendir(dirname);
+            struct dirent *dp;
+            while ((dp = readdir(dirp)) != NULL) {
+                if (strcmp(".", dp->d_name) != 0 && strcmp("..", dp->d_name) != 0) {
+                    strcpy(fname, dirname);
+                    strcat(fname, "/");
+                    strcat(fname, dp->d_name);
+                    if (unlink(fname)) {
+                        fprintf(stderr, "%s ii remove failed.\n", fname);
+                    } else {
+                        printf("%s removed.\n", fname);
+                    }
+                }
+            }
+            closedir(dirp);
+            rmdir(dirname);
+            printf("%s was removed.\n", dirname);
+        }
+    } else {
+        fprintf(stderr, "opr not recognized.\n");
+    }
     return 0;
 }
